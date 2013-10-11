a364a48eaf59426ec4f2accf0b3452dcff06c542
Mike Lynch <mike.lynch@iinet.net.au>
2013-10-11 14:55:25 +1100

    Reorganising the scripts for running ptah.pl so that they conform to the convention elsewhere observed for local config scripts: a sample in the repo with an obvious name.

--- Osiris/bin/run_ptah.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export OSIRIS=where/you/installed/it
-export OSIRIS_LIB=$OSIRIS/Osiris/lib
-export OSIRIS_PTAHLOG=$OSIRIS/Osiris/environments/ptahlog.conf
-export OSIRIS_WORKING=/where/is/working
-
-$OSIRIS/Osiris/bin/ptah.pl
