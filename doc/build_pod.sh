f64062e68d856466a56ceb0030de6732209a5126
Mike Lynch <mike.lynch@iinet.net.au>
2013-11-27 11:21:50 +1100

    Added HTML generated from the Osiris class POD, and a shell script which runs pod2html to produce it.

---
+++ doc/build_pod.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+pod2html ../Osiris/lib/Osiris.pm      > ./pod/Osiris.html
+pod2html ../Osiris/lib/Osiris/AAF.pm  > ./pod/AAF.html
+pod2html ../Osiris/lib/Osiris/App.pm  > ./pod/App.html
+pod2html ../Osiris/lib/Osiris/Form.pm > ./pod/Form.html
+pod2html ../Osiris/lib/Osiris/Job.pm  > ./pod/Job.html
+pod2html ../Osiris/lib/Osiris/Test.pm > ./pod/Test.html
+pod2html ../Osiris/lib/Osiris/User.pm > ./pod/User.html
